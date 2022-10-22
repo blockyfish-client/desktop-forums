708d78f7ff5b7845d8903dcbbd17690e19b4eaf6
pi <90981829+ThePiGuy3141@users.noreply.github.com>
2022-10-22 10:32:21 -0700

    added mica effect

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const { app, BrowserWindow, session } = require('electron')
+const { app, BrowserWindow, session, globalShortcut } = require('electron')
 const path = require('path')
 const { shell } = require("electron")
 const { ElectronBlocker } = require('@cliqz/adblocker-electron')
 const fetch = require('cross-fetch') // required 'fetch'
-const { Client } = require("discord-rpc");
+
+const { PARAMS, VALUE,  MicaBrowserWindow } = require('mica-electron');
+app.commandLine.appendSwitch("enable-transparent-visuals");
 
 ElectronBlocker.fromPrebuiltAdsAndTracking(fetch).then((blocker) => {
   blocker.enableBlockingInSession(session.defaultSession);
@@ -14,7 +16,7 @@ function matches(text, partial) {
 }
 
 const createWindow = () => {
-    const win = new BrowserWindow({
+    const win = new MicaBrowserWindow({
         width: 1080,
         height: 720,
         show: false,
@@ -23,9 +25,14 @@ const createWindow = () => {
         },
         frame: false,
         icon: path.join(__dirname, 'img/icon.png'),
+
+        effect: PARAMS.BACKGROUND.MICA,
+        theme: VALUE.THEME.DARK,
+        autoHideMenuBar: true,
     })
 
     win.loadURL('https://beta.deeeep.io/login?return=%2Fforum%2Fen')
+    // win.loadFile(path.join(__dirname, 'index.html'))
     win.removeMenu();
     setInterval(function() {
         if (win.webContents.getURL() == 'https://beta.deeeep.io/') {
@@ -36,7 +43,38 @@ const createWindow = () => {
     }, 250)
     win.webContents.on('did-finish-load', function() {
         win.webContents.openDevTools()
+        globalShortcut.register('Shift+CommandOrControl+I', () => {
+            win.webContents.toggleDevTools()
+          })
         win.webContents.setBackgroundThrottling(false)
+
+        win.webContents.executeJavaScript(`
+        const titlebar_html = document.createElement('div')
+        document.body.appendChild(titlebar_html)
+        titlebar_html.outerHTML = '<div id="window-controls"> <div class="button" id="min-button"> <img class="icon" srcset="https://blockyfish.netlify.app/assets/titlebar/min-w-10.png 1x, https://blockyfish.netlify.app/assets/titlebar/min-w-12.png 1.25x, https://blockyfish.netlify.app/assets/titlebar/min-w-15.png 1.5x, https://blockyfish.netlify.app/assets/titlebar/min-w-15.png 1.75x, https://blockyfish.netlify.app/assets/titlebar/min-w-20.png 2x, https://blockyfish.netlify.app/assets/titlebar/min-w-20.png 2.25x, https://blockyfish.netlify.app/assets/titlebar/min-w-24.png 2.5x, https://blockyfish.netlify.app/assets/titlebar/min-w-30.png 3x, https://blockyfish.netlify.app/assets/titlebar/min-w-30.png 3.5x" draggable="false"/> </div><div class="button" id="max-button"> <img class="icon" srcset="https://blockyfish.netlify.app/assets/titlebar/max-w-10.png 1x, https://blockyfish.netlify.app/assets/titlebar/max-w-12.png 1.25x, https://blockyfish.netlify.app/assets/titlebar/max-w-15.png 1.5x, https://blockyfish.netlify.app/assets/titlebar/max-w-15.png 1.75x, https://blockyfish.netlify.app/assets/titlebar/max-w-20.png 2x, https://blockyfish.netlify.app/assets/titlebar/max-w-20.png 2.25x, https://blockyfish.netlify.app/assets/titlebar/max-w-24.png 2.5x, https://blockyfish.netlify.app/assets/titlebar/max-w-30.png 3x, https://blockyfish.netlify.app/assets/titlebar/max-w-30.png 3.5x" draggable="false"/> </div><div class="button" id="restore-button" style="display: none;"> <img class="icon" srcset="https://blockyfish.netlify.app/assets/titlebar/restore-w-10.png 1x, https://blockyfish.netlify.app/assets/titlebar/restore-w-12.png 1.25x, https://blockyfish.netlify.app/assets/titlebar/restore-w-15.png 1.5x, https://blockyfish.netlify.app/assets/titlebar/restore-w-15.png 1.75x, https://blockyfish.netlify.app/assets/titlebar/restore-w-20.png 2x, https://blockyfish.netlify.app/assets/titlebar/restore-w-20.png 2.25x, https://blockyfish.netlify.app/assets/titlebar/restore-w-24.png 2.5x, https://blockyfish.netlify.app/assets/titlebar/restore-w-30.png 3x, https://blockyfish.netlify.app/assets/titlebar/restore-w-30.png 3.5x" draggable="false"/> </div><div class="button" id="close-button"> <img class="icon" srcset="https://blockyfish.netlify.app/assets/titlebar/close-w-10.png 1x, https://blockyfish.netlify.app/assets/titlebar/close-w-12.png 1.25x, https://blockyfish.netlify.app/assets/titlebar/close-w-15.png 1.5x, https://blockyfish.netlify.app/assets/titlebar/close-w-15.png 1.75x, https://blockyfish.netlify.app/assets/titlebar/close-w-20.png 2x, https://blockyfish.netlify.app/assets/titlebar/close-w-20.png 2.25x, https://blockyfish.netlify.app/assets/titlebar/close-w-24.png 2.5x, https://blockyfish.netlify.app/assets/titlebar/close-w-30.png 3x, https://blockyfish.netlify.app/assets/titlebar/close-w-30.png 3.5x" draggable="false"/> </div></div>'
+    
+        const titlebar_style = document.createElement('style')
+        document.querySelector('head').appendChild(titlebar_style)
+        titlebar_style.innerHTML = '@media (-webkit-device-pixel-ratio:1.5),(device-pixel-ratio:1.5),(-webkit-device-pixel-ratio:2),(device-pixel-ratio:2),(-webkit-device-pixel-ratio:3),(device-pixel-ratio:3){#window-controls .icon{width:10px;height:10px}}#window-controls{z-index: 9999999999999999;color:#fff;display:grid;grid-template-columns:repeat(3,46px);position:absolute;top:0;right:5px;height:32px;-webkit-app-region:no-drag}#window-controls .button{grid-row:1/span 1;display:flex;justify-content:center;align-items:center;width:50px;height:100%;user-select:none}#min-button{grid-column:1}#max-button,#restore-button{grid-column:2}#close-button{grid-column:3}#window-controls .button:hover{background:rgba(255,255,255,.1)}#window-controls .button:active{background:rgba(255,255,255,.2)}#close-button:hover{background:#e81123!important}#close-button:active{background:#bb111f!important}'
+    
+        document.getElementById("max-button").addEventListener("click", () => {
+            document.getElementById("max-button").style.display = "none"
+            document.getElementById("restore-button").style.display = ""
+            console.log('window_action: max')
+        })
+        document.getElementById("restore-button").addEventListener("click", () => {
+            document.getElementById("max-button").style.display = ""
+            document.getElementById("restore-button").style.display = "none"
+            console.log('window_action: res')
+        })
+        document.getElementById("min-button").addEventListener("click", () => {
+            console.log('window_action: min')
+        })
+        document.getElementById("close-button").addEventListener("click", () => {
+            console.log('window_action: cls')
+        })
+        `)
+
         win.webContents.executeJavaScript(`
         setInterval(function() {
             if (document.querySelector('#app > div.vfm.vfm--inset.vfm--fixed.modal > div.vfm__container.vfm--absolute.vfm--inset.vfm--outline-none.modal-container > div > div > div > div > div.nav-bar > div.forum-notifications-popover > div > span') != null) {
@@ -47,45 +85,53 @@ const createWindow = () => {
             }
         }, 5000)
         `)
+        
         win.webContents.executeJavaScript(`
         document.querySelector('#app > div.ui').remove()
-        modalCloseEvent = false
+        
         setInterval(function() {
             var forum_modal = document.querySelector('#app > div.vfm.vfm--inset.vfm--fixed.modal > div.vfm__container.vfm--absolute.vfm--inset.vfm--outline-none.modal-container > div')
             var modal_title = document.querySelector('#app > div.vfm.vfm--inset.vfm--fixed.modal > div.vfm__container.vfm--absolute.vfm--inset.vfm--outline-none.modal-container > div > span > div.justify-self-center')
-            var modal_close = document.querySelector('#app > div.vfm.vfm--inset.vfm--fixed.modal > div.vfm__container.vfm--absolute.vfm--inset.vfm--outline-none.modal-container > div > button')
             var background = document.querySelector('#app > div.vfm.vfm--inset.vfm--fixed.modal > div.vfm__overlay.vfm--overlay.vfm--absolute.vfm--inset')
-            modal_close.style.webkitAppRegion = 'no-drag'
-            if (modal_title.innerText == 'Forum') {
+            var modal_close = document.querySelector('#app > div.vfm.vfm--inset.vfm--fixed.modal > div.vfm__container.vfm--absolute.vfm--inset.vfm--outline-none.modal-container > div > button')
+            var forum_page = document.querySelector('#app > div.vfm.vfm--inset.vfm--fixed.modal > div.vfm__container.vfm--absolute.vfm--inset.vfm--outline-none.modal-container > div > div > div')
+            var forum_post_page = document.querySelector('#app > div.vfm.vfm--inset.vfm--fixed.modal > div.vfm__container.vfm--absolute.vfm--inset.vfm--outline-none.modal-container > div > div > div > div')
+            var profile_background = document.querySelector('#app > div.vfm.vfm--inset.vfm--fixed.modal > div.vfm__container.vfm--absolute.vfm--inset.vfm--outline-none.modal-container > div > div > div > div.content')
+
+            // mica effect
+            document.body.style.background = 'none'
+            forum_modal.style.background = 'none'
+            forum_page.style.background = 'none'
+            forum_post_page.style.background = 'none'
+            if (profile_background) {
+                profile_background.style.background = 'none'
+            }
+            
+            if (modal_close) {
+                modal_close.remove()
+            }
+            
+            if (modal_title.innerText != 'Login') {
+                background.style.background = 'none'
                 forum_modal.style.maxHeight = '100vh'
                 forum_modal.style.margin = '0px'
                 forum_modal.style.padding = '5px'
                 forum_modal.style.borderRadius = '0px'
                 forum_modal.style.height = '100vh'
                 forum_modal.style.width = '100vw'
+                
             }
             else {
-                forum_modal.style.maxHeight = ''
-                forum_modal.style.margin = ''
-                forum_modal.style.padding = ''
-                forum_modal.style.borderRadius = ''
-                forum_modal.style.height = ''
-                forum_modal.style.width = ''
-            }
-            background.style.backgroundImage = 'url(https://beta.deeeep.io/img/dpbg6.png)'
-            background.style.backgroundSize = 'cover'
-            background.style.backgroundPosition = 'bottom'
-            background.style.filter = 'brightness(0.8)'
-            if (modalCloseEvent == false && modal_close != null) {
-                modal_close.addEventListener("mouseup", () => {
-                    if (modal_title.innerText == 'Forum') {
-                        console.log('request_app_quit_now')
-                    }
-                })
-                modalCloseEvent = true
+                background.style.backgroundImage = 'url(https://beta.deeeep.io/img/dpbg6.png)'
+                background.style.backgroundSize = 'cover'
+                background.style.backgroundPosition = 'bottom'
+                background.style.filter = 'brightness(0.8)'
             }
         }, 100)
         `)
+
+
+
         win.webContents.executeJavaScript(`
         const drag = document.createElement('div')
         document.body.insertBefore(drag, document.body.children[0])
@@ -101,15 +147,28 @@ const createWindow = () => {
                 rect.right <= (window.innerWidth || document.documentElement.clientWidth)
             );
         }
-        document.querySelector('#app > div.vfm.vfm--inset.vfm--fixed.modal > div.vfm__container.vfm--absolute.vfm--inset.vfm--outline-none.modal-container > div > div > div').addEventListener("scroll", function() {
-            if (isInViewport(document.querySelector('#app > div.vfm.vfm--inset.vfm--fixed.modal > div.vfm__container.vfm--absolute.vfm--inset.vfm--outline-none.modal-container > div > div > div > div > div.footer > button'))) {
-                document.querySelector('#app > div.vfm.vfm--inset.vfm--fixed.modal > div.vfm__container.vfm--absolute.vfm--inset.vfm--outline-none.modal-container > div > div > div > div > div.footer > button').click()
-            }
-        })
+        setTimeout(() => {
+            setInterval(() => {
+                if (document.querySelector('#app > div.vfm.vfm--inset.vfm--fixed.modal > div.vfm__container.vfm--absolute.vfm--inset.vfm--outline-none.modal-container > div > div > div > div > div.footer > button')) {
+                    if (isInViewport(document.querySelector('#app > div.vfm.vfm--inset.vfm--fixed.modal > div.vfm__container.vfm--absolute.vfm--inset.vfm--outline-none.modal-container > div > div > div > div > div.footer > button'))) {
+                        document.querySelector('#app > div.vfm.vfm--inset.vfm--fixed.modal > div.vfm__container.vfm--absolute.vfm--inset.vfm--outline-none.modal-container > div > div > div > div > div.footer > button').click()
+                    }
+                }
+            }, 500)
+        }, 1000)
         `)
         win.webContents.on("console-message", (ev, level, message, line, file) => {
             var msg = `${message}`
-            console.log(msg);
+            // console.log(msg);
+
+            if (matches(msg, "window_action:")) {
+                msg = msg.replace("window_action: ", "")
+                if (msg == "max") win.maximize()
+                else if (msg == "res") win.unmaximize()
+                else if (msg == "min") win.minimize()
+                else if (msg == "cls") win.close()
+            }
+
             if (matches(msg, "notifs:")) {
                 if (msg.length < 10) {
                     const msg_num = msg.charAt(msg.length - 1);
@@ -130,6 +189,11 @@ const createWindow = () => {
         });
 
         win.show()
+
+        win.webContents.on('new-window', function(e, url) {
+            e.preventDefault();
+            require('electron').shell.openExternal(url);
+        });
     });
 }
 
